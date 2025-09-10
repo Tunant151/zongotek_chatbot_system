812907c9d87b472fa4632436b619e32c2c851e57
Steven Edgar Phiri <stevenphiri151@gmail.com>
2025-09-10 14:17:34 +0200

    Set base path in Vite config
    Configured the Vite project to use '/zongotek_chatbot_system/' as the base path for assets and routing.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/zongotek_chatbot_system/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
